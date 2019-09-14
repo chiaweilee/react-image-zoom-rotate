622bd7e7c76bd63a24648000a4243cdd9305f2a7
jv <ljw@live.jp>
2019-09-14 19:17:59 +0800

    feat: github docs

--- .umirc.js
+++ .umirc.js
@@ -1,5 +1,10 @@
 // ref: https://umijs.org/config/
 export default {
+  base: '/react-image-zoom-rotate/',
+  publicPath: '/react-image-zoom-rotate/',
+  cssPublicPath: '/react-image-zoom-rotate/',
+  runtimePublicPath: true,
+  outputPath: 'docs',
   chainWebpack(config /* , { webpack } */) {
     config.plugins.delete('progress');
   },
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,33 +6,40 @@ import img2 from '../assets/iu2.jpeg';
 import img3 from '../assets/iu3.jpeg';
 import img4 from '../assets/iu4.jpeg';
 import img5 from '../assets/iu5.jpeg';
-import img6 from '../assets/iu6.jpeg';
 import img7 from '../assets/iu7.jpeg';
 import img8 from '../assets/iu8.jpeg';
-import img9 from '../assets/iu9.jpeg';
 import img10 from '../assets/iu10.jpeg';
 
-const imgs = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+const imgs = [img1, img2, img3, img4, img5, img7, img8, img10];
 
 export default function() {
   const [imgIndex, setImgIndex] = useState(1);
 
-  function setIndex() {
-    setImgIndex(imgIndex + 1);
+  function setIndex(next) {
+    if (next) {
+      setImgIndex(imgIndex + 1);
+    } else {
+      setImgIndex(imgIndex - 1);
+    }
   }
 
   return (
     <div className="demo">
-      <Button onClick={setIndex}>next</Button>
+      <Button onClick={setIndex} disabled={imgIndex >= imgs.length - 1}>
+        next
+      </Button>
+      <Button onClick={setIndex.bind(null, false)} disabled={imgIndex === 1}>
+        previous
+      </Button>
       <ImageZoom
         rotate={(clockwise, antiClockwise) => {
           return (
             <div>
-              <Button onClick={clockwise} style={{ position: 'absolute', left: 0, zIndex: 1 }}>
-                顺时间
+              <Button onClick={clockwise} style={{ position: 'absolute', right: 0, zIndex: 1 }}>
+                clockwise
               </Button>
-              <Button onClick={antiClockwise} style={{ position: 'absolute', right: 0, zIndex: 1 }}>
-                逆时间
+              <Button onClick={antiClockwise} style={{ position: 'absolute', left: 0, zIndex: 1 }}>
+                anti-clockwise
               </Button>
             </div>
           );
